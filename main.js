1ada97dce5b3b6115bb62439597fbd8433b2aa9f
uopsdod <uopsdod@gmail.com>
2021-11-27 22:13:57 -0800

    add cookie response

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: false }));
 //app.use("/", main_router); /*replace index with name of router,js*/
 app.all('/api', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
