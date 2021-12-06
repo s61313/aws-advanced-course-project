dafc77dcc968486cdd8117c1c2b8817417d4319b
uopsdod <uopsdod@gmail.com>
2021-12-05 20:38:15 -0800

    add flag to enable sqs consumer

--- start.js
+++ start.js
@@ -5,5 +5,8 @@ const PORT = process.env.PORT || 8080;
 app.server.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 // sqs consumer setup 
-const sqs_queue_url = process.env.SQS_QUEUE_URL;
-app.awsSQSService.process_queue_msg_continue(sqs_queue_url)
+const is_queue_enabled_on_startup = process.env.IS_QUEUE_ENABLED_ON_STARTUP;
+if (is_queue_enabled_on_startup === 'true') {
+    const sqs_queue_url = process.env.SQS_QUEUE_URL;
+    app.awsSQSService.process_queue_msg_continue(sqs_queue_url)    
+}
