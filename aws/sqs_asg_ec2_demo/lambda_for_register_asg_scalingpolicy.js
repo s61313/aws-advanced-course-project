7ca8a141a59cbd7132176cea366eb03b5cdeea82
uopsdod <uopsdod@gmail.com>
2021-12-09 19:30:17 -0800

    update

--- aws/sqs_asg_ec2_demo/lambda_for_register_asg_scalingpolicy.js
+++ aws/sqs_asg_ec2_demo/lambda_for_register_asg_scalingpolicy.js
@@ -5,7 +5,7 @@ var asg = new AWS.AutoScaling({apiVersion: '2011-01-01'});
 exports.handler = async (event) => {
     
     var asg_name = 'asg-for-sqs-ec2';
-    var policy_name = 'scaling-policy-by-backlog-per-instace';
+    var policy_name = 'scaling-policy-by-backlog-per-instance';
     var backlog_per_instace_target = '6';
     var metric_namesapce = 'SQS_ASG';
     var metric_name = 'backlog_per_instance';
