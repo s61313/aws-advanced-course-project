fec0a0ba9efd42cadac231871302b61f579d98a3
uopsdod <uopsdod@gmail.com>
2021-12-12 19:44:11 -0800

    refactor

--- routers/elasticacheRouter.js
+++ routers/elasticacheRouter.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const awsElasticache = require("../utils/awsElasticache");
-const awsElasticacheService = new awsElasticache();
+const empModel = require("../models/empModel");
+const empModelService = new empModel();
 
 router.get("/elasticache/list_employee", async function (req, res) {
   console.log("/elasticache/list_employee called");
   // var sqs_queue_url = req.query.sqs_queue_url;
   const start_time = new Date().getTime();
-  const result = await awsElasticacheService.list_employee();
+  const result = await empModelService.list_employee();
   const end_time = new Date().getTime();
   let processed_time = (end_time - start_time) / 1000;
   res.send({
--- utils/awsElasticache.js
+++ utils/awsElasticache.js
@@ -18,41 +18,21 @@ class awsElasticache {
       return awsElasticache._instance;        
     }
 
-    list_employee() {
-      return new Promise((resolve, reject) => {
-        const sql = this.get_list_employee_sql(); 
-        // const values = [[id]];
-        const values = [];
-        mydb.getConnection()
-            .awaitQuery(sql, values)
-            .then((result) => {
-              resolve(result);
-            })
-            .catch((err) => {
-              resolve(err);
-            });
-      })
-    }    
-
-    get_list_employee_sql() {
-      const sql = 
-      `
-      SELECT e.emp_no, e.first_name, e.last_name, d.dept_name, dm.mgr_no, dm.mgr_first_name, dm.mgr_last_name
-      FROM employees e
-      LEFT JOIN dept_emp de ON e.emp_no = de.emp_no AND de.to_date = '9999-01-01'
-      LEFT JOIN departments d ON de.dept_no = d.dept_no
-      LEFT JOIN (
-        SELECT dm2.emp_no as 'mgr_no', e2.first_name as 'mgr_first_name', e2.last_name as 'mgr_last_name', dm2.dept_no
-          FROM dept_manager dm2 
-          LEFT JOIN employees e2 ON dm2.emp_no = e2.emp_no
-          WHERE dm2.to_date = '9999-01-01'
-      ) dm ON dm.dept_no = d.dept_no
-      WHERE e.emp_no != dm.mgr_no
-      ORDER BY e.emp_no
-      LIMIT 10000;
-      `
-      return sql;
-    }    
+    // list_employee() {
+    //   return new Promise((resolve, reject) => {
+    //     const sql = this.get_list_employee_sql(); 
+    //     // const values = [[id]];
+    //     const values = [];
+    //     mydb.getConnection()
+    //         .awaitQuery(sql, values)
+    //         .then((result) => {
+    //           resolve(result);
+    //         })
+    //         .catch((err) => {
+    //           resolve(err);
+    //         });
+    //   })
+    // }    
 
 }
 module.exports = awsElasticache;
