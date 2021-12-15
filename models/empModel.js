b55c272122ae437f2c046ef4d515e737c628f1fb
uopsdod <uopsdod@gmail.com>
2021-12-15 00:45:58 -0800

    update

--- models/empModel.js
+++ models/empModel.js
@@ -71,7 +71,7 @@ class EmpModel {
       ) dm ON dm.dept_no = d.dept_no
       WHERE e.emp_no != dm.mgr_no
       ORDER BY e.emp_no
-      LIMIT 100;
+      LIMIT 3;
       `
       return sql;
     }    
