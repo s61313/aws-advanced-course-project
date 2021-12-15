a48a1b358c200cb4d5547eec5fe9c9ba01033f76
uopsdod <uopsdod@gmail.com>
2021-12-15 00:18:35 -0800

    update

--- utils/awsElasticache.js
+++ utils/awsElasticache.js
@@ -141,7 +141,7 @@ class awsElasticache {
         const redis_cli_script = '/home/ec2-user/aws-advanced-course-project/redis-stable/src/redis-cli';
         var cmd_hgetall = `${redis_cli_script} -c -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} hgetall ${key}`;
         console.log("cmd_hgetall: ", cmd_hgetall);
-        let stdout_json = await this.execute_child_process(cmd_hset);
+        let stdout_json = await this.execute_child_process(cmd_hgetall);
         let stdout_obj = JSON.parse(stdout_json);
         let emp_list = [];
         for (let i = 0; i < stdout_obj.length ;i+=2) {
