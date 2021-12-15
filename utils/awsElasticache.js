08b84298065aa66dc5891157e6e6bffdbfbf16e1
uopsdod <uopsdod@gmail.com>
2021-12-14 18:37:02 -0800

    update

--- utils/awsElasticache.js
+++ utils/awsElasticache.js
@@ -73,17 +73,17 @@ class awsElasticache {
       return new Promise((resolve, reject) => {
         var redis_cli_script = '/home/ec2-user/aws-advanced-course-project/redis-stable/src/redis-cli';
         var cmd_set = `${redis_cli_script} -c -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} set ${key} ${val}`;
-        console.log("cmd_set: ", cmd_set);
+        console.log("cmd_set prefix: ", `${redis_cli_script} -c -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} set `);
         exec(cmd_set, (error, stdout, stderr) => {
           if (error) {
-              // console.log(`error: ${error.message}`);
+              console.log(`error: ${error.message}`);
               resolve();
           }
           if (stderr) {
-              // console.log(`stderr: ${stderr}`);
+              console.log(`stderr: ${stderr}`);
               resolve();
           }
-          // console.log(`stdout: ${stdout}`);
+          console.log(`stdout: ${stdout}`);
           resolve();
       });
 
