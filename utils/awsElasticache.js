20feebc7b6788737bd7f71b8ea44379a44182a45
uopsdod <uopsdod@gmail.com>
2021-12-15 00:54:47 -0800

    update

--- utils/awsElasticache.js
+++ utils/awsElasticache.js
@@ -83,13 +83,15 @@ class awsElasticache {
         // var cmd_set = `${redis_cli_script} -c -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} set ${key} '${val_json}'`;
         // var cmd_set = `${redis_cli_script} -c -x -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} set ${key} < ${jsonfilename}`;
         // console.log("cmd_set: ", cmd_set);
-        var cmd_hset = `${redis_cli_script} -c -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} hset ${key} `;
+        var cmd_hset_hash = `${redis_cli_script} -c -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} hset ${key} `;
+        var cmd_hset_key_val = '';
         for (let i = 0; i < val.length ;i++) {
           let emp = val[i];
           let emp_json = JSON.stringify(emp);
           let cmd_hset_hash_key_val = ` ${emp.emp_no} '${emp_json}'`;
-          cmd_hset += cmd_hset_hash_key_val;
+          cmd_hset_key_val += cmd_hset_hash_key_val;
         }
+        var cmd_hset = cmd_hset_hash + cmd_hset_key_val;
         console.log("cmd_hset: ", cmd_hset);
         let stdout_result = await this.execute_child_process(cmd_hset);
         resolve();
