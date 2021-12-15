b4c13dc79fa5cfec42633cbf9be468d15a349550
uopsdod <uopsdod@gmail.com>
2021-12-15 01:25:26 -0800

    update

--- utils/awsElasticache.js
+++ utils/awsElasticache.js
@@ -83,8 +83,7 @@ class awsElasticache {
         // var cmd_set = `${redis_cli_script} -c -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} set ${key} '${val_json}'`;
         // var cmd_set = `${redis_cli_script} -c -x -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} set ${key} < ${jsonfilename}`;
         // console.log("cmd_set: ", cmd_set);
-        var cmd_redis_client = `${redis_cli_script} -c -x -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} `;
-        var cmd_hset_hash = ` hset ${key} `;
+        var cmd_hset_hash = `${redis_cli_script} -c -h ${this.redis_cluster_host} -p ${this.redis_cluster_port} hset ${key} `;
         var cmd_hset_key_val = '';
         for (let i = 0; i < val.length ;i++) {
           let emp = val[i];
@@ -92,16 +91,9 @@ class awsElasticache {
           let cmd_hset_hash_key_val = ` ${emp.emp_no} '${emp_json}'`;
           cmd_hset_key_val += cmd_hset_hash_key_val;
         }
+        
         var cmd_hset = cmd_hset_hash + cmd_hset_key_val;
-        var cmd_final = `${cmd_redis_client} $cmd_hset_hash} ${cmd_hset_key_val}`;
-        console.log("cmd_final: ", cmd_final);
-        const jsonfilename = 'myjsonfile.json';
-        await myUtilService.write_to_file(jsonfilename, cmd_final);
-        // await myUtilService.read_from_file(jsonfilename);
-        
-        
-        // var cmd_hset = `${cmd_hset_hash} < ${jsonfilename}`;
-        // console.log("cmd_hset: ", cmd_hset);
+        console.log("cmd_hset: ", cmd_hset);
         let stdout_result = await this.execute_child_process(cmd_hset);
         resolve();
         
