857e7637adfdda40d37f3be7309bfcc4d28ac0d3
uopsdod <uopsdod@gmail.com>
2021-12-18 00:33:13 -0800

    update

--- public/js/elasticache_ex.js
+++ public/js/elasticache_ex.js
@@ -94,7 +94,7 @@ function simulation01Helper() {
       let names = namesList[i];
       let res = await get_employee_api(names.empName, names.mgrName);
       processed_time_total += res.processed_time;
-      $("#getEmpInfoId").html(`Get Employee Information by Two Names(${processed_time_total}s)`);
+      $("#simulation01Id").html(`Simluation(${processed_time_total}s)`);
     }
 
     resolve();
