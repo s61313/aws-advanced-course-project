1966ab0640d854a090832c7de9013af71e16d7c8
uopsdod <uopsdod@gmail.com>
2021-12-19 15:08:53 -0800

    update

--- scripts/elasticache_hw/intsall_nodejs.sh
+++ scripts/elasticache_hw/intsall_nodejs.sh
@@ -4,7 +4,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 node --version
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" 
+source ~/.bash_profile
 npm --version
