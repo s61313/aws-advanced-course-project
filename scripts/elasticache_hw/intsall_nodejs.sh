f8087315a04ce345d6fc880c3d14ec9e778a7076
uopsdod <uopsdod@gmail.com>
2021-12-19 15:35:52 -0800

    update

--- scripts/elasticache_hw/intsall_nodejs.sh
+++ scripts/elasticache_hw/intsall_nodejs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
+pwd_initial=$PWD
 cd /home/ec2-user
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
-node --version
 source ~/.bash_profile
 source ~/.bashrc
-npm --version
+cd $pwd_initial
