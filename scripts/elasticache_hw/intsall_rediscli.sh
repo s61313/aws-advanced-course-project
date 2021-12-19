52f4ceb1d556de6468a03a3cf64798d2daac70ed
uopsdod <uopsdod@gmail.com>
2021-12-19 15:33:51 -0800

    update

--- scripts/elasticache_hw/intsall_rediscli.sh
+++ scripts/elasticache_hw/intsall_rediscli.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+pwd_initial=$PWD
 cd /home/ec2-user
 sudo amazon-linux-extras install epel -y
 sudo yum install gcc jemalloc-devel openssl-devel tcl tcl-devel -y
 sudo wget http://download.redis.io/redis-stable.tar.gz
 sudo tar xvzf redis-stable.tar.gz
-cd redis-stable
+cd $PWD/redis-stable
 sudo make BUILD_TLS=yes
 echo 'export PATH=$PATH:/home/ec2-user/redis-stable/src' >> ~/.bashrc 
 source ~/.bashrc
+cd $pwd_initial
 redis-cli --version
