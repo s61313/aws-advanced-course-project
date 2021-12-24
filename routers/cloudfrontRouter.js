a19939bae001b57485bd0692856b395203078c4c
uopsdod <uopsdod@gmail.com>
2021-12-23 22:40:07 -0800

    update

--- routers/cloudfrontRouter.js
+++ routers/cloudfrontRouter.js
@@ -20,7 +20,7 @@ router.get("/cloudfront/coursevideo/signedcookie", async function (req, res) {
   const result = await awsCloudfrontService.getSignedCookies(distribution_dns);
   console.log("result.signedCookies: " , result.signedCookies);
   for(var cookieId in result.signedCookies) {
-    res.cookie(cookieId, result.signedCookies[cookieId]);
+    res.cookie(cookieId, result.signedCookies[cookieId], { domain: '.cloudfront.net', path: '/', secure: true });
   }
 
   res.send({"processed_time": myUtilService.get_process_time(start_time)});
