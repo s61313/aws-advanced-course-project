5d6033aaf0ad233ef8b1f872510d0dd77aaf2085
uopsdod <uopsdod@gmail.com>
2021-12-25 06:04:40 -0800

    update

--- public/js/cloudfront.js
+++ public/js/cloudfront.js
@@ -8,7 +8,7 @@ var color_bg_default = "rgb(36, 48, 64)";
 var rows_per_page = 500;
 var processed_time_total = 0;
 var cached_checker = new Set();
-var cf_distribution_dns = "https://mycf11.learncodebypicture.com"
+var cf_distribution_dns = "http://mycf11.learncodebypicture.com"
 let videourlall = `${cf_distribution_dns}/production/aws_cloudfront_gcp_vpc_zh.mp4`;
 // const hostname = "${BACKEND_HOST_URL}";
 
