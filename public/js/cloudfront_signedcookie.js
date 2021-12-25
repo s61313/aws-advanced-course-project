a63e23247a9b2ce0aa683ae69318aced53a7f915
uopsdod <uopsdod@gmail.com>
2021-12-25 08:43:19 -0800

    trim down

--- public/js/cloudfront_signedcookie.js
+++ public/js/cloudfront_signedcookie.js
@@ -27,13 +27,8 @@ function setUpDefault() {
 }
 
 function setupEvent(){  
-  $('#getVideoId').click({"input1": "value1"}, getVideo);
-  $('#getVideoSignedCookieId').click({"input1": "value1"}, getVideoSignedCookie); 
-  $('#getVideoBySignedCookieId').click({"input1": "value1"}, getVideoBySignedCookie); 
-  $('#getVideoBySignedCookie2Id').click({"input1": "value1"}, getVideoBySignedCookie2); 
-
-  
-
+  $('#getSignedCookieId').click({"input1": "value1"}, getSignedCookie); 
+  $('#getVideoBySignedCookie2Id').click({"input1": "value1"}, getVideoBySignedCookie2);  
 }
 
 
@@ -45,32 +40,13 @@ async function getVideoBySignedCookie2(){
   $('#getVideoBySignedCookie2Id').prop('disabled', false);
 }
 
-
-async function getVideoBySignedCookie(){
-  console.log("getVideoBySignedCookie() called");
-  $('#getVideoBySignedCookieId').prop('disabled', true);
-  $("#getVideoBySignedCookieId").html(`Get Video by Signed Cookie`);
-  await getVideoBySignedCookieHelper();  
-  $('#getVideoBySignedCookieId').prop('disabled', false);
+async function getSignedCookie(){
+  console.log("getSignedCookie() called");
+  $('#getSignedCookieId').prop('disabled', true);
+  $("#getSignedCookieId").html(`Get Signed Cookie`);
+  await getSignedCookieHelper();  
+  $('#getSignedCookieId').prop('disabled', false);
 }
-
-async function getVideoSignedCookie(){
-  console.log("getVideoSignedCookie() called");
-  $('#getVideoSignedCookieId').prop('disabled', true);
-  $("#getVideoSignedCookieId").html(`Get Video (Signed Cookie)`);
-  await getVideoSignedCookieHelper();  
-  $('#getVideoSignedCookieId').prop('disabled', false);
-}
-
-async function getVideo(){
-  console.log("getVideo() called");
-  $('#getVideoId').prop('disabled', true);
-  $("#getVideoId").html(`Get Video`);
-  // $("#simulation01Id").css("background-color", color_bg_default); 
-  await getVideoHelper();  
-  $('#getVideoId').prop('disabled', false);
-}
-
 
 function getVideoBySignedCookie2Helper() {
   return new Promise(async (resolve, reject) => {
@@ -119,45 +95,7 @@ function getVideoBySignedCookie2Helper() {
   })
 }
 
-function getVideoBySignedCookieHelper() {
-  return new Promise(async (resolve, reject) => {
-    
-    // $('#videoSrcId')[0].load(); 
-
-    let policy = getCookie('CloudFront-Policy');
-    let signature = getCookie('CloudFront-Signature');
-    let keyPairId = getCookie('CloudFront-Key-Pair-Id');
-    let videourlByCookie = `${videourlall}?Policy=${policy}&Signature=${signature}&Key-Pair-Id=${keyPairId}`;
-    console.log("videourlByCookie: ", videourlByCookie);
-    $('#videoSrcId').html("");
-    $('#videoSrcId').append(`<source src=${videourlByCookie} type="video/mp4">`);    
-    $('#videoSrcId')[0].load();    
-    resolve();
-
-    // let videourl = `${cf_distribution_dns}/aws_cloudfront_gcp_vpc_zh.mp4`;
-    // console.log("videourl: " , videourl);
-
-    // $.ajax({
-    //   url: videourl,
-    //   type: "GET",
-    //   xhrFields: {
-    //     withCredentials: true
-    //   },
-    //   success: function (res) {
-    //     console.log("videourl - res: " , res);   
-    //     // appendEmployeeRows(res.result);     
-    //     // $('#videoSrcId').html("");
-    //     // $('#videoSrcId').append(`<source src=${res.result.signedUrl} type="video/mp4">`);       
-    //     // $('#videoSrcId')[0].load(); 
-    //     resolve();
-    //   },
-    // });
-
-  })
-}
-
-
-function getVideoSignedCookieHelper() {
+function getSignedCookieHelper() {
   return new Promise(async (resolve, reject) => {
 
     let hostname = $('#backendUrlId').val();
@@ -189,34 +127,4 @@ function getVideoSignedCookieHelper() {
       },
     });
   })
-}
-
-function getVideoHelper() {
-  return new Promise(async (resolve, reject) => {
-
-    let hostname = $('#backendUrlId').val();
-    // let videourl = $('#videourl').val();
-    // let videourl = `${cf_distribution_dns}/aws_cloudfront_gcp_vpc_zh.mp4`;
-    var url_get_video = `${hostname}/api/cloudfront/coursevideo?videourl=${videourlall}`;
-    console.log("url_get_video: " , url_get_video);
-
-    $.ajax({
-      url: url_get_video,
-      type: "GET",
-      success: function (res) {
-        console.log("url_get_video - res: " , res);   
-        // appendEmployeeRows(res.result);     
-        $('#videoSrcId').html("");
-        $('#videoSrcId').append(`<source src=${res.result.signedUrl} type="video/mp4">`);       
-        $('#videoSrcId')[0].load(); 
-        resolve();
-      },
-    });
-  })
-}
-
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
 }
