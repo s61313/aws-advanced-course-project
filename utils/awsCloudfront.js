be186154730622299ec6b3cdb827c8907e0a98a8
uopsdod <uopsdod@gmail.com>
2021-12-25 04:28:18 -0800

    update

--- utils/awsCloudfront.js
+++ utils/awsCloudfront.js
@@ -64,7 +64,7 @@ MLDL2wKBgQClQWovfNn+DgW2k/8f88xDOmuyW58LnINdXWa39eAOi0JMx+4wk3/O
 tE/TsmXyiQwYf8/cVwmhNIR7Okk3Vzol/rHtXAoEBRf/U6lrHWsYpQ==
 -----END RSA PRIVATE KEY-----`;
 
-let cfUrl = "mycf09.learncodebypicture.com";
+let cfUrl = "mycf11.learncodebypicture.com";
 let expiry = Date.now() + 86400000;
 
 let policy = {
