3887278cf8c5a18082e5a4fcc589115aaf7991e5
uopsdod <uopsdod@gmail.com>
2021-12-29 01:34:43 -0800

    remove cookie in response

--- routers/cloudfrontRouter.js
+++ routers/cloudfrontRouter.js
@@ -19,17 +19,17 @@ router.get("/cloudfront/coursevideo/signedcookie", async function (req, res) {
   const start_time = new Date().getTime();
   const result = await awsCloudfrontService.getSignedCookies();
   console.log("result.signedCookies: " , result.signedCookies);
-  let cookie_array = [];
+  // let cookie_array = [];
   for(var cookieId in result.signedCookies) {
     res.cookie(cookieId, result.signedCookies[cookieId], { domain: '.learncodebypicture.com' });
-    cookie_array.push({
-      "key": cookieId,
-      "val": result.signedCookies[cookieId]
-    });
+    // cookie_array.push({
+    //   "key": cookieId,
+    //   "val": result.signedCookies[cookieId]
+    // });
   }
 
   res.send({
-    "cookie_array": cookie_array,
+    // "cookie_array": cookie_array,
     "processed_time": myUtilService.get_process_time(start_time)
   });
 });
