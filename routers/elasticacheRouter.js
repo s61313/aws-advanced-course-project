ff12b2a1a052916738232abc1cf24f660df4aeed
EC2 Default User <ec2-user@ip-10-0-4-120.ec2.internal>
2022-07-02 10:11:46 +0000

    add ex cache

--- routers/elasticacheRouter.js
+++ routers/elasticacheRouter.js
@@ -44,12 +44,19 @@ router.get("/elasticache/get_employee", async function (req, res) {
   var mgrName = req.query.mgrName;
 
   // TODO: add cache logic here
-
+  const result_cached = await awsElasticacheService.hget(empName, mgrName);
+  if (result_cached) {
+    console.log("emp_mgr_cache exists");
+    res.send({"result": result_cached,"processed_time": myUtilService.get_process_time(start_time)});
+    return;
+  }
+  
   // main logic
   console.log("cache not exists");
   const result = await empModelService.get_employees_by_empname_and_mgrname(empName, mgrName); 
 
   // TODO: add cache logic here
+  await awsElasticacheService.hset((empName, mgrName), result);
 
   res.send({"result": result,"processed_time": myUtilService.get_process_time(start_time)});
 });
